735e3d2a6fc83574e54bbaf7fafca57420132a79
mykoakbar <yeikoakbar20@gmail.com>
2024-12-11 09:53:13 +0700

    fix pohon

--- src/components/lib/Pohon/Opd/PohonOpd.tsx
+++ src/components/lib/Pohon/Opd/PohonOpd.tsx
@@ -172,9 +172,9 @@ export const PohonOpd: React.FC<pohon> = ({ tema, deleteTrigger }) => {
                         `}
                         >
                             {tema.jenis_pohon === 'Operational N' ?
-                                <h1>Operational {tema.level_pohon - 6} id: {tema.id}parent : {tema.parent}</h1>
+                                <h1>Operational {tema.level_pohon - 6}  </h1>
                                 :
-                                <h1>{tema.jenis_pohon} id: {tema.id}parent : {tema.parent}</h1>
+                                <h1>{tema.jenis_pohon} </h1>
                             }
                         </div>
                         {/* BODY */}
